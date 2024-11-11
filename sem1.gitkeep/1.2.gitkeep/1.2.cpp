a20269202eee53e96c2bfe4509f6cdffbbd8425a
Егор <egorpopov6012@gmail.com>
2024-11-11 19:06:19 +0500

    Add files via upload

---
+++ sem1.gitkeep/1.2.gitkeep/1.2.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+int main(){
+	float a, b, c;
+	cin >> a >> b;
+	c = a / b;
+	if (b != 0)
+	{
+		cout << c;
+	}
+	else
+	{
+		cout << "miss";
+	}
+	return 0;
+
+}
