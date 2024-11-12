42d5a031379755864df405e66f564666a3e6a5fb
Егор <egorpopov6012@gmail.com>
2024-11-12 18:50:11 +0500

    Add files via upload

---
+++ sem1.gitkeep/1_switch/1_switch.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int c;
+	float a, b;
+	cin >> a >> b >> c;
+	switch (c) {
+	case 1: cout << a + b << endl; break;
+	case 2: cout << a - b << endl; break;
+	case 3: cout << a * b << endl; break;
+	case 4: {
+		if (b != 0) {
+			cout << a / b << endl;
+		}
+		else {
+			cout << "division by 0" << endl;
+		}
+		break;
+	}
+	default:  cout << "-vibe" << endl;
+	}
+	return 0;
+}
