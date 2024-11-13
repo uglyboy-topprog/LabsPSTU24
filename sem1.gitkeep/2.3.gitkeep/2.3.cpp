807076b9e632ec5c749f407252a4723433280f11
Егор <egorpopov6012@gmail.com>
2024-11-13 22:29:28 +0500

    Add files via upload

---
+++ sem1.gitkeep/2.3.gitkeep/2.3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	int s = 0, t;
+	cin >> n;
+	for (int i = 1 ; i <= n; i++) {
+		t = 1;
+		for (int j = 1; j <= 2 * i; j++) {
+			t *= j;
+		}
+	}s += t;
+	cout << s << endl;
+}
