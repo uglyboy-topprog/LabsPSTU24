dfcc4c73710a479572bb54e2cd9c49b2f8b0dab6
Егор <egorpopov6012@gmail.com>
2024-11-14 15:19:45 +0500

    Add files via upload

---
+++ sem1.gitkeep/2.1.gitkeep/2.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int q;
+	int sum = 0;
+	if (q <= 0) {
+		cout << "error" << endl;
+	}
+	else {
+		for (int i = 1; i <= q; i++);
+		{
+			sum += 1;
+		}
+	}
+	cout << sum << endl;
+	return 0;
+}
