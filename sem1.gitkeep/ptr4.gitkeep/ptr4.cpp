8951ede2ca950e24243d93402981d6ff3e1e83c7
Егор <egorpopov6012@gmail.com>
2024-11-22 16:50:10 +0500

    Add files via upload

---
+++ sem1.gitkeep/ptr4.gitkeep/ptr4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n, tmp, * ptr = &tmp, max, min;
+
+
+	cout << "Введите длину последовательности: \n";
+	cin >> n; // длина последовательности
+	cout << "Введите первое число:\n";
+	cin >> *ptr;
+	max = *ptr;
+	min = *ptr;
+
+	for (int i = 2; i <= n; i++) 
+	{
+		cin >> *ptr;
+		if (*ptr > max) { max = *ptr; }
+		else if (*ptr < min) { min = *ptr; }
+	}
+	cout << "Максимальный элемент:" << max << endl;
+	cout << "Минимальный элемент:" << min << endl;
+	return  0;
+
+
+
+
+}
