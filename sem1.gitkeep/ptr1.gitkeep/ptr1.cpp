2e12143c6446f317c868b45d36372f4151bcabd6
Егор <egorpopov6012@gmail.com>
2024-11-22 16:48:59 +0500

    Add files via upload

---
+++ sem1.gitkeep/ptr1.gitkeep/ptr1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, b, t;
+	int* aptr;
+	int* bptr;
+
+	cin >> a;
+	aptr = &a;
+	t = *aptr;
+	cin >> b;
+	bptr = &b;
+	*aptr = *bptr;
+	*bptr = t;
+
+
+
+	cout << *aptr << *bptr << endl;
+	return 0;
+
+}
