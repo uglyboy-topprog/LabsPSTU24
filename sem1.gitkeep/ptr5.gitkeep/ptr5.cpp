b0a365671b36d980bc882a8515559c1e7c665437
Егор <egorpopov6012@gmail.com>
2024-11-22 16:51:30 +0500

    Add files via upload

---
+++ sem1.gitkeep/ptr5.gitkeep/ptr5.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+
+	unsigned int n, fact = 1;
+	unsigned int* f = &fact;
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) 
+	{
+		*f *= i;
+
+	}
+	cout << fact << endl;
+	return 0;
+}
