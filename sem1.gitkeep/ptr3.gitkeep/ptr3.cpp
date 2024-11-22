376a0ae80431bb19f47ca5b8bd7f211bf7dc7a18
Егор <egorpopov6012@gmail.com>
2024-11-22 16:49:53 +0500

    Add files via upload

---
+++ sem1.gitkeep/ptr3.gitkeep/ptr3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	float a, b, s1, s2;
+	float* aptr;
+	float* bptr;
+
+	cin >> a;
+	aptr = &a;
+
+	cin >> b;
+	bptr = &b;
+	s1 = *aptr;
+	s2 = *bptr;
+
+	cout << s1 + s2 << endl;
+	return 0;
+
+
+
+
+}
