eeda192dea0420eef92ebc7ea6fc0250651d9bfd
Егор <egorpopov6012@gmail.com>
2024-11-25 00:07:17 +0500

    Add files via upload

---
+++ sem1.gitkeep/2.7.gitkeep/2.7.1.cpp
@@ -0,0 +1,1 @@
+﻿﻿#include <cmath>#include<iostream>using namespace std;int main(){    int n, kr;    cin >> n;    kr = sqrt(n);    for (int i = 0; i < kr; i++)    {        for (int j = 0; j < kr; j++)        {            cout << "* ";        }        cout << endl;    }}
---
+++ sem1.gitkeep/2.7.gitkeep/2.7.3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+	for (int i = 1; i <= n - 2; i++)
+	{
+		cout << "*";
+		for (int j = 1; j <= n - 2; j++)
+		{
+			cout << " ";
+		}
+		cout << "*" << endl;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		cout << "*";
+	}
+	return 0;
+}
