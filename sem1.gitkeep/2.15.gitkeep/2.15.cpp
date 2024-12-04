0193f7d90ad1f95875596bb32229ce1162d0660e
Егор <egorpopov6012@gmail.com>
2024-12-04 16:20:20 +0500

    Add files via upload

---
+++ sem1.gitkeep/2.15.gitkeep/2.15.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{	
+	setlocale(LC_ALL, "RUS");
+	int n, s, k;
+	bool flag = false;
+	cin >> n >> s;
+	while (n > 0 && !flag)
+	{
+		k = n % 10;
+		if (k == s)
+		{
+			flag = true;
+			cout << "Цифра найдена!" << endl;
+		}
+		else { n /= 10; }
+	}
+	if (!flag) { cout << "Цифра не найдена!" << endl; }
+}
