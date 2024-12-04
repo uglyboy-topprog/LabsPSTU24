a2b901fac3dfaac62f40ae5cfa98486d1e5848fb
Егор <egorpopov6012@gmail.com>
2024-12-04 16:21:19 +0500

    Add files via upload

---
+++ sem1.gitkeep/2.18.gitkeep/2.18.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int result = 0;
+	cin >> n;
+	while (n > 0)
+	{
+		result = result * 10 + n % 10;
+		n /= 10;
+	}
+	cout << result << endl;
+}
