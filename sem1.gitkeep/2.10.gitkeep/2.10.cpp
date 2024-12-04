1895fb31be9daaaa6d27d028547ecfec784e3934
Егор <egorpopov6012@gmail.com>
2024-12-04 16:16:32 +0500

    Add files via upload

---
+++ sem1.gitkeep/2.10.gitkeep/2.10.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int n, tmp, max;
+	cout << "Введите длину последовательности:" << endl;
+	cin >> n;
+	cout << "Введите первое число:" << endl;
+	cin >> max;
+	for (int i = 2; i <= n; i++)
+	{
+		cout << "Введите следующее число : " << endl;
+			cin >> tmp;
+		if (tmp > max) { max = tmp; }
+	}
+	cout << "Максимальный элемент: " << max << endl;
+}
