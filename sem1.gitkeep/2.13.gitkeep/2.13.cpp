ea1e3078e1d5de6e5c8c5b1c3849e46046bdf28e
Егор <egorpopov6012@gmail.com>
2024-12-04 16:17:57 +0500

    Add files via upload

---
+++ sem1.gitkeep/2.13.gitkeep/2.13.cpp
@@ -0,0 +1,24 @@
+﻿//Решите квадратное уравнение вида a ∗ x2 + b ∗ x + c = 0. Коэффициенты уравнения вводятся с клавиатуры.
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int a, b, c;
+	double x1, x2;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	if (b * b - 4 * a * c < 0)
+	{
+		cout << "error" << endl;
+	}
+	else if (b * b - 4 * a * c >= 0)
+	{
+		x1 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+		x2 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+	}
+	cout << "Превый корень уравнения:" << x1 << endl;
+	cout << "Второй корень уравнения:" << x2 << endl;
+}
