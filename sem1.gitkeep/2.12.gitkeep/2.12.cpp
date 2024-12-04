63256aa44ccca8394d2f7725b0c9b9c5aa0c343c
Егор <egorpopov6012@gmail.com>
2024-12-04 16:17:31 +0500

    Add files via upload

---
+++ sem1.gitkeep/2.12.gitkeep/2.12.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{	
+	setlocale(LC_ALL, "RUS");
+	int n, b, max_0 = -10000, min = 10000;
+	cout << "Введите длину последовательности:" << endl;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) 
+	{	
+		cout << "Введите - " << n << " - элемента(ов) последовательности:" << endl;
+		cin >> b;
+		if (b >= max_0) 
+		{
+			max_0 = b;
+		}
+		if (b <= min) 
+		{
+			min = b;
+		}
+	}
+	cout << "Сумма минимального и максимального значения:" << endl;
+	cout << max_0 + min << endl;
+}
