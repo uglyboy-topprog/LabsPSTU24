b708e2c29663b0e6b94fa8c2e65c765d57f082c7
Егор <egorpopov6012@gmail.com>
2024-12-21 16:55:19 +0500

    Add files via upload

---
+++ sem1.gitkeep/eq_Newton.gitkeep/eq_Newton.cpp
@@ -0,0 +1,21 @@
+﻿// f'(x)=0.75x**2 +1
+// f''(x)=1.5x
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	float x, x1;
+
+	x = 2;
+	x1 = 0;
+	double eps = 0.000001;
+
+	while (abs(x - x1) > eps)
+	{
+		x1 = x;
+		x = x1 - (0.25 * (pow(x1, 3)) + x1 - 1.2502) / (0.75 * (pow(x1, 3)) + 1);
+	}
+	cout << x << endl;
+	return 0;
+}
