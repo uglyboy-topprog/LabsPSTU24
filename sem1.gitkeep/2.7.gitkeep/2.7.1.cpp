06a98d39c6b2b36f6d3405e07a35b43dfed78496
Егор <egorpopov6012@gmail.com>
2024-12-21 13:31:20 +0500

    2.7.1.cpp

--- sem1.gitkeep/2.7.gitkeep/2.7.1.cpp
+++ sem1.gitkeep/2.7.gitkeep/2.7.1.cpp
@@ -1,1 +1,18 @@
-﻿﻿#include <cmath>#include<iostream>using namespace std;int main(){    int n, kr;    cin >> n;    kr = sqrt(n);    for (int i = 0; i < kr; i++)    {        for (int j = 0; j < kr; j++)        {            cout << "* ";        }        cout << endl;    }}
+﻿﻿#include <cmath>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, kr;
+    cin >> n;
+    kr = sqrt(n);
+    for (int i = 0; i < kr; i++)
+    {
+        for (int j = 0; j < kr; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
