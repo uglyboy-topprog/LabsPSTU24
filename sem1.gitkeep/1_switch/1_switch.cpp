fd46db034563b143a490c193cacc1a5db844d88b
Егор <egorpopov6012@gmail.com>
2024-12-23 21:55:51 +0500

    1_switch.cpp Исправленный

--- sem1.gitkeep/1_switch/1_switch.cpp
+++ sem1.gitkeep/1_switch/1_switch.cpp
@@ -4,20 +4,39 @@ int main() {
 	int c;
 	float a, b;
 	cin >> a >> b >> c;
-	switch (c) {
-	case 1: cout << a + b << endl; break;
-	case 2: cout << a - b << endl; break;
-	case 3: cout << a * b << endl; break;
-	case 4: {
-		if (b != 0) {
+	switch (c) 
+	{
+	case 1:
+	{
+		cout << a + b << endl; 
+		break;
+	}
+	case 2:
+	{
+		cout << a - b << endl; 
+		break;
+	}
+	case 3:
+	{
+		cout << a * b << endl; 
+		break;
+	}
+	case 4:
+	{
+		if (b != 0) 
+		{
 			cout << a / b << endl;
 		}
-		else {
+		else
+		{
 			cout << "division by 0" << endl;
 		}
 		break;
 	}
-	default:  cout << "-vibe" << endl;
+	default:
+	{
+		cout << "-vibe" << endl;
+	}
 	}
 	return 0;
 }
