3a6563691b20609acef6cfc13c378a3d9bc3323f
Егор <egorpopov6012@gmail.com>
2024-12-24 16:04:25 +0500

    Add files via upload

---
+++ sem1.gitkeep/2.20.gitkeep/2.20.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	float n, found, tmp;
+	bool flag = false;
+	cin >> n >> found;
+	for (int i = 1; i <= n; i++)
+	{
+		tmp = sin(n + (i / n));
+		if (found == tmp)
+		{
+			flag = true;
+		}
+	}
+	if (flag)
+	{
+		cout << "Элемент найден" << endl;
+	}
+	else
+	{
+		cout << "Элемент не найден" << endl;
+	}
+	return 0;
+}
