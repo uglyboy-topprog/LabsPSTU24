1edf2b38d9150c7485cbb5f0e93fe106190cb292
Егор <egorpopov6012@gmail.com>
2024-12-24 16:05:38 +0500

    Add files via upload

---
+++ sem1.gitkeep/2.21.gitkeep/2.21.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float tmp, previous;
+    bool increas = true, decreas = true;
+
+    cout << "Введите первое число, не 0 \n";
+    cin >> tmp;
+    previous = tmp;
+    while (true)
+    {
+        cin >> tmp;
+        if (tmp == 0)
+        {
+            break;
+        }
+        else
+        {
+            if (tmp < previous)
+            {
+                increas = false;
+            }
+            else if (tmp > previous)
+            {
+                decreas = false;
+            }
+            previous = tmp;
+        }
+    }
+    if (increas)
+        cout << "Числа упорядочены по возрастанию\n";
+    else if (decreas)
+        cout << "Числа упорядочены по убыванию\n";
+    else
+        cout << "Числа не упорядочены\n";
+
+    return 0;
+}
