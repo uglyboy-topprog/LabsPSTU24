cd46b453f1218fe3b7e299ed5ff68cf0572fdab8
Егор <egorpopov6012@gmail.com>
2024-12-24 16:07:12 +0500

    Add files via upload

---
+++ sem1.gitkeep/2.22.gitkeep/2.22.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float n, tmp, nomer = 0, chislo = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        tmp = sin(n + i / n);
+        if (tmp > chislo)
+        {
+            chislo = tmp;
+            nomer = i;
+        }
+    }
+    cout << "Наибольшее число " << chislo << ", его номер в последовательности " << nomer << endl;
+    return 0;
+}
