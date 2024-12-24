6dc0883e794928fbd63c1758155e7d768760510a
Егор <egorpopov6012@gmail.com>
2024-12-24 16:08:45 +0500

    Add files via upload

---
+++ sem1.gitkeep/2.23.gitkeep/2.23.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float n, tmp, max = 0, count = -1;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        tmp = sin(n + i / n);
+        if (tmp > max)
+        {
+            max = tmp;
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        tmp = sin(n + i / n);
+        if (tmp == max)
+        {
+            count++;
+        }
+    }
+    cout << "Наибольшее число " << max << ", совпадений " << count << endl;
+    return 0;
+}
