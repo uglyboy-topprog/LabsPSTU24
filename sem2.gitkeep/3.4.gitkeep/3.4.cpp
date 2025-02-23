5cc04ebf953345d4c3369c8a1bfee79a85e0d6e1
Егор <egorpopov6012@gmail.com>
2025-02-23 14:11:21 +0500

    Add files via upload

---
+++ sem2.gitkeep/3.4.gitkeep/3.4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int n = 5;
+	bool flag = true;
+	int arr[n] = { 3,2,5,7,8 };
+
+	for (int i = n - 1; i > 0; i--)
+	{
+		if (arr[i] < arr[i - 1])
+		{
+			flag = false;
+		}
+	}
+
+	if (flag)
+	{
+		cout << "yep" << endl;
+	}
+	else
+	{
+		cout << "nono" << endl;
+	}
+	return 0;
+}
