cb198ee4bebb8e80d66a58dcf2239e5012ed96a6
Егор <egorpopov6012@gmail.com>
2025-02-23 13:37:04 +0500

    Add files via upload

---
+++ sem2.gitkeep/lab_7.1.gitkeep/lab 7.1.cpp
@@ -0,0 +1,40 @@
+﻿// Вариант 10
+
+#include <iostream>
+using namespace std;
+
+float diff(float a, float b) 
+{
+	return a - b;
+}
+
+double diff(double a, double b) 
+{
+	return a - b;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	float a;
+	cout << "Введите первое число" << endl;
+	cin >> a;
+
+	float b;
+	cout << "Введите второе число" << endl;
+	cin >> b;
+
+	cout << "Разность:" << diff(a, b) << endl;
+
+	double c;
+	cout << "Введите первое число" << endl;
+	cin >> c;
+
+	double d;
+	cout << "Введите второе число" << endl;
+	cin >> d;
+
+	cout << "Разность" << diff(a, b) << endl;
+	return 0;
+}
