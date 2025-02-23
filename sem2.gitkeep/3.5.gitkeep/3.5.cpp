e54046bc0282c77674796deeef35f2b0a911b96d
Егор <egorpopov6012@gmail.com>
2025-02-23 14:11:57 +0500

    Add files via upload

---
+++ sem2.gitkeep/3.5.gitkeep/3.5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int n = 5;
+	int arr[n] = { 1,2,3,4,5 };
+	int max = arr[0];
+
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+	cout << max << endl;
+	return 0;
+}
