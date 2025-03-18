b4df841a5f7b5f16337293905a6047f3e9f221ba
Егор <egorpopov6012@gmail.com>
2025-03-18 13:29:27 +0500

    Add files via upload

---
+++ sem2.gitkeep/lab_5.gitkeep/lab_5.cpp
@@ -0,0 +1,43 @@
+﻿﻿#include <iostream>
+using namespace std;
+int neg_str(int arr[3][3], int num, int size) {
+	bool flag = 1; // проверяем, имеет строка отрицательные элементы или нет 
+
+	for (int i = 0; i < 3; i++) {
+		if (arr[num][i] < 0) {
+			flag = 0;
+			break;
+		}
+	}
+	return flag;
+}
+int main() {
+	int size;
+
+	cin >> size;
+	int arr[3][3] = { {-1,2,0},{1,-1,1},{3,2, 1} };
+
+	for (int i = 0; i < size; cout << endl, i++)
+		for (int j = 0; j < size; cout << arr[i][j] << " ", j++);
+	cout << endl;
+
+	for (int i = 0; i < size; i++) {
+		if (neg_str(arr, i, size) == 0) {
+			for (int k = i; k < size - 1; k++) {
+				for (int j = 0; j < size; j++) {
+					arr[k][j] = arr[k + 1][j]; // значение текущего элемента заменяем на значение следующего элемента в следующей строке
+				}
+			}
+			for (int j = 0; j < size; j++) {
+				arr[size - 1][j] = 0; // обнуляет последнюю строку 
+			}
+			i--;
+			for (int i = 0; i < size; cout << endl, i++)
+				for (int j = 0; j < size; cout << arr[i][j] << " ", j++);
+			cout << endl;
+		}
+
+	}
+
+	return 0;
+}
