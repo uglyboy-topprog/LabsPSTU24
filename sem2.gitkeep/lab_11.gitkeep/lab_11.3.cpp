2537afba5c01ffe2eb746f5182a5e182605111a7
Егор <egorpopov6012@gmail.com>
2025-03-18 13:31:26 +0500

    Add files via upload

---
+++ sem2.gitkeep/lab_11.gitkeep/lab_11.3.cpp
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+struct Node
+{
+    char data;
+    Node* next = nullptr;
+    Node* prev = nullptr;
+};
+
+struct List {
+    Node* head = nullptr;
+    Node* tail = nullptr;
+};
+
+void pushBack(List& list, const char& data)
+{
+    Node* new_node = new Node;
+    new_node->data = data;
+    if (list.head == nullptr)
+    {
+
+        list.head = new_node;
+        list.tail = new_node;
+    }
+    else {
+        list.tail->next = new_node;
+        new_node->prev = list.tail;
+        list.tail = new_node;
+    }
+}
+
+void pushFront(List& list, const char& data) {
+    Node* new_node = new Node;
+    new_node->data = data;
+    if (list.head == nullptr)
+    {
+
+        list.head = new_node;
+        list.tail = new_node;
+    }
+    else {
+        new_node->next = list.head;
+        list.head->prev = new_node;
+        list.head = new_node;
+    }
+}
+
+void print(List& list) {
+    Node* current_node = list.head;
+    if (current_node == nullptr)
+    {
+        cout << "Ваш список оказался пустым" << endl;
+    }
+    else {
+        cout << "Ваш список: " << endl;
+        while (current_node != nullptr)
+        {
+            cout << current_node->data << " ";
+            current_node = current_node->next;
+        }
+        cout << endl;
+    }
+}
+
+
+void remove(List& list, char& key) {
+    Node* current_node = new Node;
+    current_node->next = list.head;
+    while (current_node != nullptr)
+    {
+        Node* tmp = current_node;
+        current_node = current_node->next;
+        if (tmp->data == key)
+        {
+            if (tmp->next == nullptr && tmp->prev == nullptr)
+            {
+
+                list.head = nullptr;
+                list.tail = nullptr;
+            }
+            else if (tmp == list.head)
+            {
+                list.head = tmp->next;
+                list.head->prev = nullptr;
+            }
+            else if (tmp == list.tail)
+            {
+                list.tail = tmp->prev;
+                list.tail->next = nullptr;
+            }
+            else {
+                tmp->prev->next = tmp->next;
+                tmp->next->prev = tmp->prev;
+            }
+
+            delete tmp;
+        }
+    }
+}
+
+int main() {
+    system("chcp 1251");
+    int n;
+    cout << "Введите количество элементов списка: " << endl;
+    cin >> n;
+    List list;
+    for (int i = 0; i < n; i++) {
+        char Char;
+        cout << "Введите символ: " << endl;
+        cin >> Char;
+        pushBack(list, Char);
+    }
+    cout << endl;
+    print(list);
+    char key;
+    cout << "Введите символ, который нужно удалить: " << endl;
+    cin >> key;
+    remove(list, key);
+    print(list);
+    cout << "Введите сколько еще символов вы хотите добавить в конец списка: " << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        char Char;
+        cout << "Введите символ: " << endl;
+        cin >> Char;
+        pushBack(list, Char);
+    }
+    cout << endl;
+    print(list);
+}
---
+++ sem2.gitkeep/lab_11.gitkeep/lab_11.4.cpp
@@ -0,0 +1,163 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+struct Node
+{
+    T data;
+    Node<T>* next;
+};
+
+template <typename T>
+struct Queue
+{
+    int size;
+    Node<T>* head;
+    Node<T>* tail;
+};
+
+template <typename T>
+void newQueue(Queue<T>& q, int n) {
+    T a;
+    cout << "Введите элемент: ";
+    cin >> a;
+    initQueue(q, a);
+    for (int i = 2; i <= n; i++) {
+        cout << "Введите элемент: ";
+        cin >> a;
+        push(q, a);
+    }
+}
+
+template <typename T>
+void initQueue(Queue<T>& q, const T& value) {
+    Node<T>* new_Node = new Node<T>();
+    new_Node->data = value;
+    q.head = new_Node;
+    q.tail = new_Node;
+    q.size = 1;
+}
+
+template <typename T>
+void push(Queue<T>& q, const T& value) {
+    Node<T>* new_Node = new Node<T>();
+    q.size++;
+    new_Node->data = value;
+    new_Node->next = nullptr;
+    q.tail->next = new_Node;
+    q.tail = new_Node;
+}
+
+template <typename T>
+void printQueue(Queue<T>& q) {
+    Node<T>* tmp = q.head;
+    cout << "начало -> ";
+    while (tmp != nullptr)
+    {
+        cout << tmp->data << " -> ";
+        tmp = tmp->next;
+    }
+    cout << "конец" << endl;
+}
+
+template <typename T>
+void pop(Queue<T>& q)
+{
+    Node<T>* tmp = q.head;
+    q.head = q.head->next;
+    q.size--;
+    delete tmp;
+}
+
+template <typename T>
+void deleteKey(Queue<T>& q, T k)
+{
+    int i = 1;
+    while (i <= q.size)
+    {
+        if (q.head->data == k)
+        {
+            pop(q);
+        }
+        else {
+            push(q, q.head->data);
+            pop(q);
+            i++;
+        }
+    }
+}
+
+template <typename T>
+void insert(Queue<T>& q, int count, int number)
+{
+    int i = 1;
+    while (i < number)
+    {
+        push(q, q.head->data);
+        pop(q);
+        i++;
+    }
+    T a;
+    for (int j = 1; j <= count; j++) {
+        cout << "Введите элемент для добавления: ";
+        cin >> a;
+        push(q, a);
+    }
+    for (i; i <= q.size - count; i++)
+    {
+        push(q, q.head->data);
+        pop(q);
+    }
+}
+
+template <typename T>
+void deleteQueue(Queue<T>& q)
+{
+    while (q.head->next != nullptr)
+    {
+        pop(q);
+    }
+    Node<T>* tmp = q.head;
+    q.head = nullptr;
+    q.size--;
+    delete tmp;
+}
+
+int main()
+{
+    system("chcp 1251");
+    Queue<char> q;
+    int n;
+
+    do
+    {
+        cout << "Введите количество элементов в очереди: ";
+        cin >> n;
+    } while (n <= 0);
+
+    newQueue(q, n);
+    cout << endl;
+    printQueue(q);
+
+    cout << endl << "Введите ключ элемента, который необходимо удалять: ";
+    char key;
+    cin >> key;
+
+    deleteKey(q, key);
+    cout << endl;
+    printQueue(q);
+
+    int number = q.size + 1;
+    cout << endl << "Введите количество элементов для добавления: ";
+    int count;
+    cin >> count;
+
+    insert(q, count, number);
+    cout << endl;
+    printQueue(q);
+
+    cout << endl << "Очищение динамической памяти ...";
+    deleteQueue(q);
+    cout << "Завершено." << endl;
+    printQueue(q);
+    return 0;
