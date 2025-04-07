a2e046b78c0294e9abd79fcc30b81f1345670399
Егор <egorpopov6012@gmail.com>
2025-04-07 11:30:52 +0500

    Update hoara sort.cpp

--- sem2.gitkeep/sorting methods.gitkeep/hoara sort.cpp
+++ sem2.gitkeep/sorting methods.gitkeep/hoara sort.cpp
@@ -1,17 +1,21 @@
 ﻿#include <iostream>
 using namespace std;
-void swap(int& a, int& b) {
+void swap(int& a, int& b) 
+{
     int temp = a;
     a = b;
     b = temp;
 }
 
-int HoaraSort(int arr[], int low, int high) {
+int HoaraSort(int arr[], int low, int high) 
+{
     int pivot = arr[high];
     int i = low - 1;
 
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
+    for (int j = low; j <= high - 1; j++) 
+    {
+        if (arr[j] < pivot) 
+        {
             i++;
             swap(arr[i], arr[j]);
         }
@@ -21,8 +25,10 @@ int HoaraSort(int arr[], int low, int high) {
     return i + 1;
 }
 
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
+void quickSort(int arr[], int low, int high) 
+{
+    if (low < high) 
+    {
         int pivot = HoaraSort(arr, low, high);
         quickSort(arr, low, pivot - 1);
         quickSort(arr, pivot + 1, high);
@@ -36,7 +42,8 @@ int main()
     int arr[] = { 1, 0, 2, 0, 3, 0, 0, 0, 1, 1, 1, 2, 2, 3, 4, 5, 5, 5, 3, 4, 2, 0, 2, 3, 4 };
 
     cout << "Исходный массив: ";
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) 
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
@@ -44,7 +51,8 @@ int main()
     quickSort(arr, 0, size - 1);
 
     cout << "Отсортированный массив: ";
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) 
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
