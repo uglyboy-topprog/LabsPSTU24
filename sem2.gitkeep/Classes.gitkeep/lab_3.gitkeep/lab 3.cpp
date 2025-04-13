03b27c6eafdce713ebe544dc9fb89b26c6e45005
Егор <egorpopov6012@gmail.com>
2025-04-13 12:21:16 +0500

    Add files via upload

---
+++ sem2.gitkeep/Classes.gitkeep/lab_3.gitkeep/lab 3.cpp
@@ -0,0 +1,74 @@
+﻿// main 
+
+#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+int main() {
+	Money A, B, C;
+	cin >> A >> B;
+	++A;
+
+	cout << A << endl;
+	C = (A++) + B;
+
+	cout << "A = " << A << endl;
+	cout << "B = " << B << endl;
+	cout << "C = " << C << endl;
+	return 0;
+}
+
+// Money.cpp
+
+#include <iostream>
+#include "Money.h"
+
+using namespace std;
+
+Money& Money::operator =(const Money& temp) {
+	if (&temp == this) {
+		return *this;
+	}
+	rub = temp.rub;
+	kop = temp.kop;
+	return *this;
+}
+
+Money& Money::operator ++() {
+	int temp = rub * 100 + kop;
+	temp++;
+	kop = temp / 100;
+	kop = temp % 100;
+	return *this;
+}
+
+Money Money::operator ++(int) {
+	int temp = rub * 100 + kop;
+	temp++;
+	Money ex1(rub, kop);
+	kop = temp / 100;
+	kop = temp % 100;
+	return ex1;
+}
+
+Money Money::operator +(const Money& temp) {
+	int ex1 = rub * 100 + kop;
+	int ex2 = temp.rub * 100 + temp.kop;
+	Money temp2;
+	temp2.rub = (ex1 + ex2) / 100;
+	temp2.kop = (ex1 + ex2) % 60;
+	return temp2;
+}
+
+istream& operator >>(istream& in, Money& temp) {
+	cout << "rubles: ";
+	in >> temp.rub;
+	cout << "kopecks: ";
+	in >> temp.kop;
+	return in;
+}
+
+ostream& operator <<(ostream& out, const Money& temp) {
+	return (out << temp.rub << ", " << temp.kop);
+}
---
+++ sem2.gitkeep/Classes.gitkeep/lab_3.gitkeep/lab 3.h
@@ -0,0 +1,29 @@
+﻿// Money.h
+
+#pragma once
+#include <iostream> 
+using namespace std;
+class Money
+{
+	long roubles;
+	int penny;
+public:
+	Money() { roubles = 0; penny = 0; };
+	Money(long r, int p) { roubles = r; penny = p; }
+	Money(const  Money& t) { roubles = t.roubles; penny = t.penny; }
+	~Money() {};
+	int getRoubles() { return roubles; }
+	int getPenny() { return penny; }
+	void setRoubles(long r) { roubles = r; }
+	void setPenny(int p) { penny = p; }
+
+	//перегруженные операции 
+	Money& operator=(const Money&);
+	Money& operator++();
+	Money operator++(int);//постфиксная операция 
+	Money operator+(const Money&);
+
+	//глобальные функции ввода-вывода
+	friend istream& operator>>(istream& in, Money& t);
+	friend ostream& operator<<(ostream& out, const Money& t);
+};
