c18bbca430f11bb9f1726c380de9c6af2092bb59
Егор <egorpopov6012@gmail.com>
2025-04-13 12:15:07 +0500

    Add files via upload

---
+++ sem2.gitkeep/Classes.gitkeep/lab_2.gitkeep/lab 2.cpp
@@ -0,0 +1,128 @@
+﻿// main 
+
+#include "Car.h" 
+#include <iostream> 
+#include <string> 
+using namespace std;
+
+//функция для возврата объекта как результата 
+Car makeCar()
+{
+	string b;
+	string m;
+	int c;
+	cout << "Enter brand: ";
+	cin >> b;
+	cout << "Enter model: ";
+	cin >> m;
+	cout << "Enter cost: ";
+	cin >> c;
+	Car t(b, m, c);
+	return t;
+}
+
+//функция для передачи объекта как параметра 
+void printCar(Car t)
+{
+	t.show();
+}
+
+void main()
+{
+	//конструктор без параметров 
+	Car t1;
+	t1.show();
+
+	//коструктор с параметрами 
+	Car t2("Lada ", "Vesta", 1500000);
+	t2.show();
+
+	//конструктор копирования 
+	Car t3 = t2;
+	t3.setBrand("Mersedes");
+	t3.setModel("Benz");
+	t3.setCost(5000000);
+
+	//конструктор копирования 
+	printCar(t3);
+
+	//конструктор копирования 
+	t1 = makeCar();
+	t1.show();
+}
+
+// Car.cpp
+
+#include "Car.h" 
+#include <iostream> 
+#include <string> 
+using namespace std;
+
+//конструктор без параметров 
+Car::Car()
+{
+	brand = "";
+	model = "";
+	cost = 0;
+	cout << "Constructor without parameters " << this << endl;
+}
+
+//конструктор с параметрами 
+Car::Car(string N, string K, int S)
+{
+	brand = N;
+	model = K;
+	cost = S;
+	cout << "Constructor with parameters " << this << endl;
+}
+
+//конструктор копирования 
+Car::Car(const Car& t)
+{
+	brand = t.brand;
+	model = t.model;
+	cost = t.cost;
+	cout << "Copy constructor " << this << endl;
+}
+
+//деструктор 
+Car::Car()
+{
+	cout << "Destructor " << this << endl;
+}
+
+//селекторы
+string Car::getBrand()
+{
+	return brand;
+}
+string Car::getModel()
+{
+	return model;
+}
+int Car::getCost()
+{
+	return cost;
+}
+
+//модификаторы
+void Car::setBrand(string N)
+{
+	brand = N;
+}
+void Car::setModel(string K)
+{
+	model = K;
+}
+void Car::setCost(int S)
+{
+	cost = S;
+}
+
+//метод для просмотра атрибутов 
+void Car::show()
+{
+	cout << "Brand :" << brand << endl;
+	cout << "Model :" << model << endl;
+	cout << "Cost :" << cost << endl;
+}
---
+++ sem2.gitkeep/Classes.gitkeep/lab_2.gitkeep/lab 2.h
@@ -0,0 +1,28 @@
+﻿// Car.h
+
+#pragma once
+#include <iostream> 
+#include <string> 
+using namespace std;
+
+class Car {
+	string brand;
+	string model;
+	int cost;
+public:
+	Car();//конструктор без параметров
+	Car(string, string, int);//конструктор с параметрами 
+	Car(const Car&);//конструктор копирования
+	~Car();//деструктор
+
+	string getBrand();//селектор
+	void setBrand(string);//модификатор 
+
+	string getModel();//селектор
+	void setModel(string); //модификатор
+
+	int getCost();//селектор
+	void setCost(int); //модификатор 
+
+	void show();//просмотр атрибутов
+};
