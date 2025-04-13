4d9f9aca36c9cf12c4b2fb58ec61c0c0883ae50f
Егор <egorpopov6012@gmail.com>
2025-04-13 12:07:00 +0500

    Add files via upload

---
+++ sem2.gitkeep/Classes.gitkeep/lab_1.gitkeep/lab 1.cpp
@@ -0,0 +1,53 @@
+﻿// main
+
+#include <iostream>
+#include "fraction.h"
+using namespace std;
+
+fraction make_fraction(double F, int S)
+{
+    fraction t;//создали временную переменную
+    t.Init(F, S);//инициализировали поля переменной t с помощью параметров функции 
+    return t;//вернули значение переменной t
+}
+
+int main() 
+{
+    fraction A;
+    A.Init(1, 1);
+    A.Read();
+    A.Show();
+    cout << A.Summa();
+
+    return 0;
+}
+
+// fraction.cpp
+
+#include <iostream>
+#include "fraction.h"
+using namespace std;
+
+void fraction::Init(double F, int S)
+{
+    first = F; second = S;
+}
+
+void fraction::Read()
+{
+    cout << "Salary: "; cin >> first;
+    cout << "Days worked: "; cin >> second;
+}
+
+void fraction::Show()
+{
+    cout << "Salary: " << first;
+    cout << "Days worked: " << second;
+    cout << "\n";
+}
+
+double fraction::Summa()
+{
+    return (first * second) / 30;
+}
+
