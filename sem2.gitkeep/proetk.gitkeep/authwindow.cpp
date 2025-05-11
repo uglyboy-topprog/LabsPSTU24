89b4717f16880a73737b72d5b59ee957fe1503b3
Егор <egorpopov6012@gmail.com>
2025-05-11 15:31:58 +0500

    Update authwindow.cpp

--- sem2.gitkeep/proetk.gitkeep/authwindow.cpp
+++ sem2.gitkeep/proetk.gitkeep/authwindow.cpp
@@ -1,55 +1,53 @@
 #include "authwindow.h"
-#include "loginwindow.h"
-#include "registrationwindow.h"
-#include "mainwindow.h"
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QDebug>
 
-AuthWindow::AuthWindow(QWidget *parent) : QWidget(parent)
-{
-    setWindowTitle("Authentication");
-    resize(300, 200);
+AuthWindow::AuthWindow(QWidget *parent) : QWidget(parent) {
+    setWindowTitle("Окно авторизации");
+    setMinimumSize(200, 120);
 
     QVBoxLayout *layout = new QVBoxLayout(this);
+    QLabel *titleLabel = new QLabel("Добро пожаловать!", this);
+    titleLabel->setAlignment(Qt::AlignCenter); // Центрируем текст
+    loginButton = new QPushButton("Вход", this);
+    registrationButton = new QPushButton("Регистрация", this);
 
-    QPushButton *loginBtn = new QPushButton("Вход", this);
-    QPushButton *registerBtn = new QPushButton("Регистрация", this);
+    layout->addWidget(titleLabel);
+    layout->addWidget(loginButton);
+    layout->addWidget(registrationButton);
 
-    layout->addWidget(loginBtn);
-    layout->addWidget(registerBtn);
+    setLayout(layout);
+    adjustSize();
 
-    connect(loginBtn, &QPushButton::clicked, this, &AuthWindow::showLogin);
-    connect(registerBtn, &QPushButton::clicked, this, &AuthWindow::showRegistration);
+    connect(loginButton, &QPushButton::clicked, this, &AuthWindow::showLoginWindow);
+    connect(registrationButton, &QPushButton::clicked, this, &AuthWindow::showRegistrationWindow);
+
+    qDebug() << "AuthWindow создан";
 }
 
-AuthWindow::~AuthWindow()
-{
-    delete loginWindow;
-    delete registrationWindow;
-    delete mainWindow;
-}
-
-void AuthWindow::showLogin()
-{
+void AuthWindow::showLoginWindow() {
+    qDebug() << "Открываем окно входа";
     if (!loginWindow) {
-        loginWindow = new LoginWindow();
-        connect(loginWindow, &LoginWindow::authenticated, this, &AuthWindow::showLanguageWindow);
+        qDebug() << "Создаём LoginWindow";
+        loginWindow = new LoginWindow(nullptr);
+        qDebug() << "LoginWindow создан успешно";
     }
     loginWindow->show();
+    loginWindow->raise();
+    loginWindow->activateWindow();
+    qDebug() << "LoginWindow отображено:" << loginWindow->isVisible();
 }
 
-void AuthWindow::showRegistration()
-{
+void AuthWindow::showRegistrationWindow() {
+    qDebug() << "Открываем окно регистрации";
     if (!registrationWindow) {
-        registrationWindow = new RegistrationWindow();
+        qDebug() << "Создаём RegistrationWindow";
+        registrationWindow = new RegistrationWindow(nullptr);
+        qDebug() << "RegistrationWindow создан успешно";
     }
     registrationWindow->show();
-}
-
-void AuthWindow::showLanguageWindow()
-{
-    this->hide();
-
-    if (!mainWindow) {
-        mainWindow = new MainWindow();
-    }
-    mainWindow->showLanguageSelection();
+    registrationWindow->raise();
+    registrationWindow->activateWindow();
+    qDebug() << "RegistrationWindow отображено:" << registrationWindow->isVisible();
 }
