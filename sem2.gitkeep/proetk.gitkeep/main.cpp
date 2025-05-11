7fd3edc859ade62fbdc4bca7d6080d7cab656e14
Егор <egorpopov6012@gmail.com>
2025-05-11 15:35:42 +0500

    Update main.cpp

--- sem2.gitkeep/proetk.gitkeep/main.cpp
+++ sem2.gitkeep/proetk.gitkeep/main.cpp
@@ -1,10 +1,11 @@
-#include "authwindow.h"
 #include <QApplication>
+#include "authwindow.h"
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    AuthWindow w;
-    w.show();
-    return a.exec();
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    AuthWindow authWindow;
+    authWindow.show();
+
+    return app.exec();
 }
