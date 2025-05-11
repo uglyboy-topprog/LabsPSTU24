8d64c9e90c4ee8e94fec195d69cd178f844f13ac
Егор <egorpopov6012@gmail.com>
2025-05-11 15:36:05 +0500

    Update mainwindow.cpp

--- sem2.gitkeep/proetk.gitkeep/mainwindow.cpp
+++ sem2.gitkeep/proetk.gitkeep/mainwindow.cpp
@@ -1,106 +1,68 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
 #include <QVBoxLayout>
+#include <QLabel>
+#include <QComboBox>
 #include <QPushButton>
-#include <QListWidget>
-#include <QDebug> // Добавили для qDebug()
-
+#include <QWidget>
+#include <QDebug>
+#include "difficultywindow.h"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     setWindowTitle("Main Window");
-    resize(300, 200);
-    stackedWidget = new QStackedWidget(this);
-    setCentralWidget(stackedWidget);
-    setupLanguagePage();
-    setupDifficultyPage();
-    setupContentPage(0);  // Передаем 0 в качестве значения по умолчанию
+    setMinimumSize(200, 150);
+
+    QWidget *centralWidget = new QWidget(this);
+    setCentralWidget(centralWidget);
+
+    QVBoxLayout *layout = new QVBoxLayout(centralWidget);
+
+    infoLabel = new QLabel("Выберите язык: Русский, English, বাংলা", centralWidget);
+    languageChoice = new QComboBox(centralWidget);
+    languageChoice->addItem("Русский", "ru");
+    languageChoice->addItem("English", "en");
+    languageChoice->addItem("বাংলা", "bn");
+
+    QPushButton *okButton = new QPushButton("OK", centralWidget);
+    QPushButton *cancelButton = new QPushButton("Назад", centralWidget);
+
+    layout->addWidget(infoLabel);
+    layout->addWidget(languageChoice);
+    layout->addWidget(okButton);
+    layout->addWidget(cancelButton);
+
+    connect(okButton, &QPushButton::clicked, this, [this]() {
+        handleLanguageChoice(languageChoice->currentData().toString());
+    });
+    connect(cancelButton, &QPushButton::clicked, this, [this]() {
+        qDebug() << "Cancel нажата, возвращаемся к AuthWindow";
+        this->hide();
+    });
+
+    qDebug() << "MainWindow создан";
 }
 
 void MainWindow::showLanguageSelection() {
-    stackedWidget->setCurrentIndex(0);
-    show();
+    qDebug() << "Показываем выбор языка";
+    this->show();
+    this->raise();
+    this->activateWindow();
+    qDebug() << "MainWindow видимость после show:" << isVisible();
 }
 
-void MainWindow::setupLanguagePage() {
-    QWidget *page = new QWidget();
-    QVBoxLayout *layout = new QVBoxLayout(page);
-    QStringList languages = {"Английский", "Китайский", "Испанский"};
-    for (const QString &lang : languages) {
-        QPushButton *button = new QPushButton(lang, page);
-        connect(button, &QPushButton::clicked, this, [this, lang]() {
-            onLanguageSelected(lang);
-        });
-        layout->addWidget(button);
-    }
-    stackedWidget->addWidget(page);
-}
-
-void MainWindow::setupDifficultyPage() {
-    QWidget *page = new QWidget();
-    QVBoxLayout *layout = new QVBoxLayout(page);
-
-    for (int i = 1; i <= 5; ++i) {
-        QPushButton *button = new QPushButton(QString::number(i), this);
-        connect(button, &QPushButton::clicked, this, [this, i]() {
-            onButtonClicked(i); // Передаём корректный buttonId (1, 2, 3, 4, 5)
-        });
-        layout->addWidget(button);
+void MainWindow::handleLanguageChoice(const QString &language) {
+    qDebug() << "Выбран язык:" << language;
+    if (language == "ru") {
+        infoLabel->setText("Вы выбрали русский язык");
+    } else if (language == "en") {
+        infoLabel->setText("You selected English");
+    } else if (language == "bn") {
+        infoLabel->setText("আপনি বাংলা ভাষা নির্বাচন করেছেন");
     }
 
-    stackedWidget->addWidget(page);
-}
-
-void MainWindow::onButtonClicked(int buttonId) {
-    qDebug() << "Button clicked:" << buttonId; // Для отладки
-    setupContentPage(buttonId);
-    stackedWidget->setCurrentIndex(2); // Переключаем на страницу с контентом
-}
-
-
-void MainWindow::setupContentPage(int buttonId) {
-    QWidget *page = new QWidget();
-    QVBoxLayout *layout = new QVBoxLayout(page);
-    QListWidget *list = new QListWidget(page);
-
-    QStringList items;
-    switch (buttonId) {
-        case 1:
-            items << "Item A" << "Item B" << "Item C";
-            break;
-        case 2:
-            items << "Item X" << "Item Y" << "Item Z";
-            break;
-        case 3:
-            items << "Item 3A" << "Item 3B" << "Item 3C";
-            break;
-        case 4:
-            items << "Item 4X" << "Item 4Y" << "Item 4Z";
-            break;
-        case 5:
-            items << "Item 5A" << "Item 5B" << "Item 5C";
-            break;
-        default:
-            items << "Default 1" << "Default 2" << "Default 3";
-            break;
+    if (!difficultyWindow) {
+        difficultyWindow = new DifficultyWindow(language, this, nullptr);
     }
-
-    list->addItems(items);
-    layout->addWidget(list);
-    stackedWidget->addWidget(page);
+    difficultyWindow->show();
+    difficultyWindow->raise();
+    difficultyWindow->activateWindow();
 }
-
-void MainWindow::onLanguageSelected(const QString &lang) {
-    Q_UNUSED(lang);
-    stackedWidget->setCurrentIndex(1);
-}
-
-void MainWindow::onDifficultySelected(int level) {
-    Q_UNUSED(level);
-    stackedWidget->setCurrentIndex(2);
-}
-
-void MainWindow::showDifficultySelection() {
-    stackedWidget->setCurrentIndex(1);
-}
-
-
