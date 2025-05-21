7753d82087af60c7f0425ed65c76710f8fd3b2a6
Егор <egorpopov6012@gmail.com>
2025-05-21 17:21:21 +0500

    Add files via upload

---
+++ sem2.gitkeep/Classes.gitkeep/lab_7.gitkeep/lab_7.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include "list.h"
+#include "money.h"
+
+using namespace std;
+
+int main() {
+	int k = 1;
+	int size = 5;
+	List<int> A(size);
+	cout << "A: "; A.show();
+	cout << "A[k]: " << A[k] << endl;
+	cout << "A: " << A() << endl;
+
+	List<int> B(size);
+	cout << "B: "; B.show();
+	cout << "B: " << B() << endl;
+
+	List<int> C(size);
+	C = A + B;
+	cout << "C = A + B: "; C.show();
+
+	/////////////
+	Money<long, int> a(10203040, 110);
+	a.show();
+	Money<long, int> b(1000000, 21);
+	b.show();
+
+	return 0;
+}
---
+++ sem2.gitkeep/Classes.gitkeep/lab_7.gitkeep/money.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include <iostream>
+#include "list.h"
+
+using namespace std;
+
+template <class T, class T1>
+class Money {
+private:
+	T rub;
+	T kop;
+public:
+	Money(long, int);
+	~Money();
+	Money(const Money&);
+	Money operator =(const Money&);
+	Money operator +(const Money&);
+	void show();
+};
+
+template <class T, class T1>
+Money<T, T1>::Money(long R, int K) {
+	rub = R;
+	kop = K;
+}
+
+template <class T, class T1>
+Money<T, T1>::~Money() {}
+
+template <class T, class T1>
+Money<T, T1> Money<T, T1>::operator =(const Money& money) {
+	rub = money.rub;
+	kop = money.kop;
+}
+
+template <class T, class T1>
+void Money<T, T1>::show() {
+	cout << "\nResult money: " << rub << ", " << kop;
+}
+
+template <class T, class T1>
+Money<T, T1>Money<T, T1>::operator +(const Money& money) {
+	int M = rub * 100 + kop;
+	int monM = money.rub * 100 + money.kop;
+	M += monM;
+
+	Money temp(M / 100, M % 100);
+	return temp;
+}
