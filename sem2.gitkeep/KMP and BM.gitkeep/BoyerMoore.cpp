81e1931e1931c0d7ef6cfa39e119431f5d783d7b
Егор <egorpopov6012@gmail.com>
2025-05-21 17:44:58 +0500

    Add files via upload

---
+++ sem2.gitkeep/KMP and BM.gitkeep/BoyerMoore.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+const int CHAR_NUM = 256;
+
+void calcCharTable(string pat, int size, int charTable[CHAR_NUM])
+{
+    for (int i = 0; i < CHAR_NUM; i++)
+    {
+        charTable[i] = -1;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        charTable[(int)pat[i]] = i;
+    }
+}
+
+void boyerMooreSearch(string str, string pat)
+{
+    int strSize = str.size();
+    int patSize = pat.size();
+    int shiftTable[CHAR_NUM];
+    calcCharTable(pat, patSize, shiftTable);
+    int shift = 0;
+    while (shift <= (strSize - patSize))
+    {
+        int j = patSize - 1;
+        while (j >= 0 && pat[j] == str[shift + j])
+        {
+            j--;
+        }
+        if (j < 0)
+        {
+            cout << "Подстрока найдена на позиции: " << shift << endl;
+            if (shift + patSize < strSize)
+            {
+                shift += patSize -
+                    shiftTable[(int)str[shift + patSize]];
+            }
+        }
+        else
+        {
+            shift += max(1, j - shiftTable[(int)str[shift + j]]);
+        }
+    }
+}
+
+int main()
+{
+    string inputStr, searchStr;
+    cin >> inputStr;
+    cin >> searchStr;
+    boyerMooreSearch(inputStr, searchStr);
+    return 0;
+}
---
+++ sem2.gitkeep/KMP and BM.gitkeep/Knuth-Morris-Pratt.cpp
@@ -0,0 +1,73 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int* calcPrefixFunc(string pat, int size)
+{
+    int* lps = new int[size]; // longest prefix-seffix
+    int len = 0;
+    lps[0] = 0;
+    int i = 1;
+
+    while (i < size)
+    {
+        if (pat[i] == pat[len])
+        {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else
+        {
+            if (len != 0) { len = lps[len - 1]; }
+            else { lps[i] = 0; i++; }
+        }
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << lps[i];
+    }
+    cout << endl;
+    return lps;
+}
+
+void knutMorrisPrattSearch(string str, string pat)
+{
+    int strSize = str.size();
+    int patSize = pat.size();
+
+    int* lps = calcPrefixFunc(pat, patSize);
+
+    int strIdx = 0;
+    int patIdx = 0;
+
+    while ((strSize - strIdx) >= (patSize - patIdx))
+    {
+        if (pat[patIdx] == str[strIdx])
+        {
+            patIdx++;
+            strIdx++;
+        }
+
+        if (patIdx == patSize)
+        {
+            cout << "Подстрока найдена по индкесу: " << strIdx - patIdx << endl;
+            patIdx = lps[patIdx - 1];
+        }
+        else if (strIdx < strSize && pat[patIdx] != str[strIdx])
+        {
+            if (patIdx != 0) { patIdx = lps[patIdx - 1]; }
+            else { strIdx++; }
+        }
+    }
+}
+
+int main()
+{
+    string inputStr, searchStr;
+    cin >> inputStr;
+    cin >> searchStr;
+    knutMorrisPrattSearch(inputStr, searchStr);
+    return 0;
+}
